f6d4a78c33b8504a102125b5cc182e342242e70d
Holly <hollies@hollies.pw>
2017-02-19 16:30:05 -0800

    Version 2.2.2 Fixes critical error in 2.1.1 from syntax error.

--- content-scripts/listener.js
+++ content-scripts/listener.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
     modules.filterUsers.perform(
       settings.filterUsers,
       filteredUsers,
-      settings.logging,);
+      settings.logging);
 
     modules.showFilterComments.perform(
       settings.showFilteredComments,
