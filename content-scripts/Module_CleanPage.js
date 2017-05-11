35211827c91b9afbcd7f66a83bda8d9ae26824c4
Holly <hollies@hollies.pw>
2017-05-11 11:50:20 -0700

    Changes for version 2.3.3 - handle new article format - add link to online help

--- content-scripts/Module_CleanPage.js
+++ content-scripts/Module_CleanPage.js
@@ -4,6 +4,8 @@
 function Module_CleanPage() {
 
   var loggingEnabled = false;
+  var pageFormat = -1;
+  var selector = "";
 
   return {
     perform: perform,
@@ -27,6 +29,12 @@ function Module_CleanPage() {
   //    filter: selector of siblings not to remove
   //    exclude: exclude siblings with this text in tag, id, or class
   function removeSiblings(selector, filter, exclude) {
+    if ($(selector).find("#livefyre_comment_stream").length > 0) {
+      // alert("Attemt to remove forum!");
+      // never remove the forum!
+      return;
+    }
+
     var toRemove = filter ?
       $(selector).siblings().not(filter) : $(selector).siblings();
 
@@ -56,8 +64,54 @@ function Module_CleanPage() {
     }
   }
 
+    // add a link to online help if not present
+  function addHelpLink(text) {
+  
+    if (!$("#ffh-help").length) {
+        $("<div id='ffh-help' style='float:right;vertical-align:middle;margin-top:-5px' ></div>")
+            .insertBefore("#livefyre_comment_stream .fyre-stream-header");
+
+      const _browser = window.browser ? window.browser : window.chrome;
+      var manifest = _browser.runtime.getManifest();
+      var url = "http://hollies.pw/static/ffh/" + manifest.version + "/help/"
+
+      var link = $(" \
+            <a style='margin-right: 20px;color: \
+            #0000FF;font-size: 12px;' target='_blank'></a>");
+      link.appendTo("#ffh-help");
+      link.text(text);
+      link.attr("href", url);
+    }
+  }
+
+  function hideArticle() {
+    if ($("div .article-content").length) {
+      $("div .article-content").hide();
+    } else {
+      $('article').first().hide();
+    }
+  }
+
+  function showArticle() {
+    if ($("div .article-content").length) {
+      $("div .article-content").show();
+    } else {
+      $('article').first().show();
+    }
+  }
+
   // install - install the feature
   function install(removeMasthead, removeVideo, customLinkTitle, customLinkUrl) {
+
+   if ($('#content .main').length) {
+     pageFormat = 1;
+     selector = '#content .main';
+   } else if ($('div .page-content').length) {
+     pageFormat = 2;
+     selector = 'div .page-content';
+   } else {
+     alert("unknown article format: cannot clean page");
+   }
 
     removeSiblings('#wrapper', '#templateHolder', 'janrain');
 
@@ -67,9 +121,17 @@ function Module_CleanPage() {
       removeSiblings('#doc', "notdef", 'masthead ');
     }
 
-    removeSiblings('#content .main');
+    removeSiblings(selector);
     removeSiblings('#commenting', 'article');
     $(".freeform").remove();
+
+    // addtional cleaning for alernate article format
+    $(".outbrain").remove();
+    $("header > .meta").remove();
+    $("header > .social-count").remove();
+    $(".article-sidebar").remove();
+    $(".site-footer").remove();
+    $(".pre-content").remove();
 
     if (removeVideo) {
       $(".video-container").remove();
@@ -94,7 +156,13 @@ function Module_CleanPage() {
     // fix issue in Fox News CSS where the main editor and the user avatar overlap making
     // it imposible to select with the mouse the first dozen or so letters in a comment
     $('#livefyre_comment_stream .fyre-editor').first().css('padding-top', 16);
-    $('article').first().hide();
+
+    if (pageFormat==2) {
+      // set the formum text size
+      $('head').append('<style> .fyre .fyre-comment p { font-size: 17px; } </style>');
+    }
+
+    hideArticle();
 
     // add custom link
     if (customLinkTitle && customLinkTitle != "" &&
@@ -102,19 +170,31 @@ function Module_CleanPage() {
       addCustomLink(customLinkTitle, customLinkUrl);
     }
 
+    addHelpLink("Help for Fox Forum Helper");
+
+
     // add buttons div
-    $('#content .main').prepend("<div id='ffhButtons'>");
+    if (pageFormat==1) {
+      $(selector).prepend("<div id='ffhButtons'>");
+    } else if (pageFormat==2) {
+      $("header.article-header").prepend("<div id='ffhButtons'>");
+    }
 
     // add button to toggle article display
     $('#ffhButtons').append("<button id='toggleArticle'>Show Article</button>");
-    $('#toggleArticle').css('margin', '20px 20px 0px 0px');
+    if (pageFormat==1) {
+      $('#toggleArticle').css('margin', '20px 20px 0px 0px');
+    } else if (pageFormat==2) {
+      $('#toggleArticle').css('margin', '20px 20px 20px 0px');
+    }    
+
     $('#toggleArticle').click(function () {
       if ($(this).text() == 'Show Article') {
-        $('article').first().show();
+        showArticle();
         $(this).text('Hide Article');
         $(this).css('margin-bottom', '20px');
       } else {
-        $('article').first().hide();
+        hideArticle();
         $(this).text('Show Article');
         $(this).css('margin-bottom', '0px');
       }
